982b244025ffae51d21d12f949b544e29bbd813a
Marifer082 <137328871+Marifer082@users.noreply.github.com>
2023-07-19 12:55:23 -0600

    Se crea el menú principal Se crea la interfaz para controlar el nuvel de la energia

--- app/src/main/java/com/example/iot_house/MainActivity2.java
+++ app/src/main/java/com/example/iot_house/MainActivity2.java
@@ -3,12 +3,56 @@ package com.example.iot_house;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class MainActivity2 extends AppCompatActivity {
+    ImageView ivnagua, ivgaraje, ivluz, ivventilador, ivriego;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+
+        ivnagua = (ImageView) findViewById(R.id.ivnagua);
+        ivgaraje = (ImageView) findViewById(R.id.ivgaraje);
+        ivluz = (ImageView) findViewById(R.id.ivluz);
+        ivventilador = (ImageView) findViewById(R.id.ivventilador);
+        ivriego = (ImageView) findViewById(R.id.ivriego);
+
+        ivnagua.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        ivgaraje.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        ivluz.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        ivventilador.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        ivriego.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
     }
 }

792b6b1d5ec7d2ffc2423829e41730288793a7cb
Marifer082 <137328871+Marifer082@users.noreply.github.com>
2023-07-24 12:38:52 -0600

    Se crea el menú principal Se crea la interfaz para controlar el nuvel de la energia

--- app/src/main/java/com/example/iot_house/MainActivity3.java
+++ app/src/main/java/com/example/iot_house/MainActivity3.java
@@ -3,12 +3,38 @@ package com.example.iot_house;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.SeekBar;
+import android.widget.TextView;
 
 public class MainActivity3 extends AppCompatActivity {
+    TextView tvnivel;
+    SeekBar sbluz;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main3);
+
+        tvnivel = (TextView) findViewById(R.id.tvnivel);
+        sbluz = (SeekBar) findViewById(R.id.sbluz);
+
+        sbluz.setMax(10);
+        sbluz.setProgress(0);
+        sbluz.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+                tvnivel.setText(String.valueOf(i));
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
     }
 }

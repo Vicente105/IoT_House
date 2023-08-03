b1a53a30f8896dd31388ab5e9699f8850b7dd67b
Marifer082 <137328871+Marifer082@users.noreply.github.com>
2023-08-03 15:40:57 -0600

    Agregamos codigo para el control de la luz

--- app/src/main/java/com/example/iot_house/MainActivity5.java
+++ app/src/main/java/com/example/iot_house/MainActivity5.java
@@ -3,12 +3,17 @@ package com.example.iot_house;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity5 extends AppCompatActivity {
+
+    TextView tvnivea;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main5);
+
+        tvnivea = (TextView) findViewById(R.id.tvnivea);
     }
 }

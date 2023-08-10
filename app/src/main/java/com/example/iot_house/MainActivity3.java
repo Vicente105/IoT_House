cb416f2c9450f0eb1aa710676f5319b1d9934bb7
Marifer082 <137328871+Marifer082@users.noreply.github.com>
2023-08-10 17:57:20 -0600

    Agregamos codigo al control de la luz electrica

--- app/src/main/java/com/example/iot_house/MainActivity3.java
+++ app/src/main/java/com/example/iot_house/MainActivity3.java
@@ -27,9 +27,10 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class MainActivity3 extends AppCompatActivity {
-    private ImageView btnEncender, btnApagar;
-    private String apiUrl = "http://192.168.100.13"; // Cambiar por la URL de tu API
+    private ImageView btnEncender, btnApagar, btnControlEsp;
     private RequestQueue requestQueue;
+    private String apiUrl = "https://gopestudiante.000webhostapp.com/api_domotica/update_v_app.php"; // Cambiar por la URL de tu API
+    private String espUrl = "http://192.168.1.100"; // Cambiar por la IP del ESP8266
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,46 +40,66 @@ public class MainActivity3 extends AppCompatActivity {
         btnEncender = (ImageView) findViewById(R.id.btnencender);
         btnApagar = (ImageView) findViewById(R.id.btnapagar);
 
+
         requestQueue = Volley.newRequestQueue(this);
 
         btnEncender.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                enviarEstadoFoco(1, "Encender");
+                enviarEstadoFocoApi(1, "Encender");
+                enviarComandoEsp("encender");
             }
         });
 
         btnApagar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                enviarEstadoFoco(0, "Apagar");
+                enviarEstadoFocoApi(0, "Apagar");
+                enviarComandoEsp("apagar");
             }
         });
     }
 
-    private void enviarEstadoFoco(int estado, String accion) {
+    private void enviarEstadoFocoApi(int estado, String accion) {
         StringRequest stringRequest = new StringRequest(Request.Method.POST, "https://gopestudiante.000webhostapp.com/api_domotica/update_v_app.php",
                 new Response.Listener<String>() {
                     @Override
                     public void onResponse(String response) {
-                        Toast.makeText(MainActivity3.this, accion, Toast.LENGTH_SHORT).show();
+                        Toast.makeText(MainActivity3.this, accion + " - API", Toast.LENGTH_SHORT).show();
                     }
                 },
                 new Response.ErrorListener() {
                     @Override
                     public void onErrorResponse(VolleyError error) {
-                        Toast.makeText(MainActivity3.this, "Error. Intente más tarde", Toast.LENGTH_SHORT).show();
+                        Toast.makeText(MainActivity3.this, "Error en la API. Intente más tarde", Toast.LENGTH_SHORT).show();
                     }
                 }) {
 
             @Override
             protected Map<String, String> getParams() {
-                Map<String, String> parametros = new HashMap();
+                Map<String, String> parametros = new HashMap<>();
                 parametros.put("pkSensor", "2");
-                parametros.put("v_app", estado+""); // 1 o 0
+                parametros.put("v_app", estado + ""); // 1 o 0
                 return parametros;
             }
         };
+        requestQueue.add(stringRequest);
+    }
+
+    private void enviarComandoEsp(String comando) {
+        StringRequest stringRequest = new StringRequest(Request.Method.GET, espUrl + "/" + comando,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        Toast.makeText(MainActivity3.this, "Comando enviado al ESP8266: " + comando, Toast.LENGTH_SHORT).show();
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Toast.makeText(MainActivity3.this, "Error al enviar comando al ESP8266", Toast.LENGTH_SHORT).show();
+                    }
+                });
         requestQueue.add(stringRequest);
     }
 }
